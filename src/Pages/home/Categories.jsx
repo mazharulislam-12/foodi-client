9ec21eb1af2322a0fa75d4184eedc0b24fd2f68a
Mazharul Islam <505mazharulislam@gmail.com>
2024-02-20 21:32:10 +0600

    categories part crate

--- src/Pages/home/Categories.jsx
+++ src/Pages/home/Categories.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+
+const categoryItems = [
+    { id: 1, title: "Main Dish", despriction: "(86 dishes)", image: "/images/home/category/img1.png" },
+    { id: 2, title: "Break Fast", despriction: "(12 break fast)", image: "/images/home/category/img2.png" },
+    { id: 3, title: "Dessert", despriction: "(48 dessert)", image: "/images/home/category/img3.png" },
+    { id: 4, title: "Browse All", despriction: "(255 Items)", image: "/images/home/category/img4.png" }
+]
+
+
 const Categories = () => {
     return (
         <div className='section-container py-16'>
@@ -7,6 +16,28 @@ const Categories = () => {
                 <h5 className='sub-title'>Customer Favorites</h5>
                 <h2 className='title'>Popular Catagories</h2>
             </div>
+
+
+            {/* category cards */}
+            <div className='flex flex-col sm:flex-row flex-wrap justify-around gap-8 items-center m-12 rounded-md'>
+                {
+                    categoryItems.map((item, i) => (
+                        <div key={i} className='shadow-lg rounded-md bg-white py-6 px-5 w-72 mx-auto text-center cursor-pointer hover:-translate-y-4 duration-300 transition-all'>
+                            <div className='flex w-full mx-auto items-center justify-center'>
+                                <img src={item.image} alt="" className='bg-[#C1F1C6] p-5 rounded-full w-28 h-28' />
+                            </div>
+                            <div className='mt-5 space-y-1' >
+                                <h5>{item.title}</h5>
+                                <p>{item.despriction}</p>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+
+
+
+
         </div>
     );
 };
