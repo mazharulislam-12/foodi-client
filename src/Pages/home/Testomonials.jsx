17a490c0759b5036c6e1285e5360cd12407ff7f1
Mazharul Islam <505mazharulislam@gmail.com>
2024-02-22 01:16:20 +0600

    Customer Feedback section

--- src/Pages/home/Testomonials.jsx
+++ src/Pages/home/Testomonials.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaStar } from 'react-icons/fa'
 
 const Testomonials = () => {
     return (
@@ -11,7 +12,40 @@ const Testomonials = () => {
                     <div className='text-left md:w-4/5'>
                         <h5 className='sub-title'>Testimonials</h5>
                         <h2 className='title md:w-[450px] '>What Our Customers Say About Us</h2>
-                        <p className='text-[#555]'>“I had the pleasure of dining at Foodi last night, and I'm still raving about the experience! The attention to detail in presentation and service was impeccable”</p>
+                        <p className='text-secondary my-5 leading-[30px]'>“I had the pleasure of dining at Foodi last night, and I'm still raving about the experience! The attention to detail in presentation and service was impeccable”</p>
+
+                        {/* avator section */}
+                        <div className='flex items-center gap-6 flex-wrap'>
+                            <div className="avatar-group -space-x-6 rtl:space-x-reverse">
+                                <div className="avatar">
+                                    <div className="w-12">
+                                        <img src="/images/home/testimonials/testimonial1.png" />
+                                    </div>
+                                </div>
+                                <div className="avatar">
+                                    <div className="w-12">
+                                        <img src="/images/home/testimonials/testimonial2.png" />
+                                    </div>
+                                </div>
+                                <div className="avatar">
+                                    <div className="w-12">
+                                        <img src="/images/home/testimonials/testimonial3.png" />
+                                    </div>
+                                </div>
+                                <div className="avatar placeholder">
+                                    <div className="w-12 bg-neutral text-neutral-content">
+                                        <span>+99</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='space-y-1'>
+                                <h5 className='text-lg font-semibold'>Customer Feedback</h5>
+                                <div>
+                                    <FaStar className='text-yellow-400'></FaStar>
+                                    <span className='flex items-center font-medium' >4.9 <span className='text-[#807E7E]'>(18.6K Reviews)</span> </span>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
