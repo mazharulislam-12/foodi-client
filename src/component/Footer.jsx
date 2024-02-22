32fe03c717631fb8ed0a41576b397d658309e984
Mazharul Islam <505mazharulislam@gmail.com>
2024-02-22 14:48:25 +0600

    copyright section

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,34 +1,45 @@
 import React from 'react';
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <div>
-            <footer className="footer p-10 bg-base-200 text-base-content mt-12">
-                <aside>
-                    <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-                    <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
-                </aside>
+            <footer className="footer grid grid-cols-4 items-center p-10 bg-base-200 text-base-content mt-12">
+                <div className='ml-8'>
+                    <img src="/logo.png" alt="" />
+                    <p>Savor the artistry where <br /> every dish is a culinary <br /> masterpiece</p>
+                </div>
                 <nav>
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
-                </nav>
-                <nav>
-                    <h6 className="footer-title">Company</h6>
+                    <h6 className="footer-title ">Useful links</h6>
                     <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <a className="link link-hover">Events</a>
+                    <a className="link link-hover">Blogs</a>
+                    <a className="link link-hover">FAQ</a>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <h6 className="footer-title">Main Menu</h6>
+                    <a className="link link-hover">Home</a>
+                    <a className="link link-hover">Offers</a>
+                    <a className="link link-hover">Menus</a>
+                    <a className="link link-hover">Reservation</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Contact Us</h6>
+                    <a className="link link-hover">example@email.com</a>
+                    <a className="link link-hover">+64 958 248 966</a>
+                    <a className="link link-hover">Social media</a>
                 </nav>
             </footer>
+
+            <div className='flex items-center gap-[400px] my-4'>
+                <div className='flex items-center gap-6 text-2xl ml-12 cursor-pointer '>
+                    <FaFacebook></FaFacebook>
+                    <FaInstagram></FaInstagram>
+                    <FaTwitter></FaTwitter>
+                    <FaYoutube></FaYoutube>
+                </div>
+                <div className='items-center'><span className='text-red'>Copyright &copy; 2023 Dscode | All rights reserved</span></div>
+            </div>
         </div>
     );
 };
