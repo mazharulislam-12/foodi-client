e32a877859f643dea51f3569f757f870192dd9f2
Mazharul Islam <505mazharulislam@gmail.com>
2024-03-24 14:58:36 +0600

    menu shop create

--- src/Pages/shop/Menu.jsx
+++ src/Pages/shop/Menu.jsx
@@ -1,99 +1,174 @@
-import React, { useEffect, useState } from 'react';
-
+import { useEffect, useState } from "react";
+import Cards from "../../component/Cards";
+import "./Menu.css";
+import { FaFilter } from "react-icons/fa";
 
 const Menu = () => {
-    const [menu, setMenu] = useState([]);
-    const [filteredItems, setFilteredItems] = useState([]);
-    const [selectedCategory, setSelectedCategory] = useState("all");
-    const [sortOption, setSortOption] = useState("default")
+  const [menu, setMenu] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [sortOption, setSortOption] = useState("default");
 
+  // loading data
+  useEffect(() => {
+    //fetch data form the backend
+    const fetchData = async () => {
+      try {
+        const response = await fetch("menu.json");
+        const data = await response.json();
+        // console.log(data);
+        setMenu(data);
+        setFilteredItems(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    // call the function
+    fetchData();
+  }, []);
 
-    // loading data
-    useEffect(() => {
-        //fetch data form the backend
-        const fetchData = async () => {
-            try {
-                const response = await fetch("menu.json")
-                const data = await response.json()
-                // console.log(data);
-                setMenu(data)
-                setFilteredItems(data)
-            }
-            catch (error) {
-                console.log(error);
-            }
-        }
-        // call the function
-        fetchData()
+  // filtering data based on category
+  const filterItem = (category) => {
+    const filtered =
+      category === "all"
+        ? menu
+        : menu.filter((item) => item.category === category);
+    setFilteredItems(filtered);
+    setSelectedCategory(category);
+  };
 
-    }, [])
+  // show all data function
+  const showAll = () => {
+    setFilteredItems(menu);
+    setSelectedCategory("all");
+  };
 
-    // filtering data base on category
-    const filterItem = (category) => {
-        const filtered = category === "all" ? menu : menu.filter((item) => item.category === category);
-        setFilteredItems(filtered);
-        setSelectedCategory(category);
+  // sorting based on A-Z, Z-A, Low-High price
+  const handleSortChange = (option) => {
+    setSortOption(option);
+    let sortedItems = [...filteredItems];
+
+    // logic
+    switch (option) {
+      case "A-Z":
+        sortedItems.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case "Z-A":
+        sortedItems.sort((a, b) => b.name.localeCompare(a.name));
+        break;
+      case "low-to-high":
+        sortedItems.sort((a, b) => a.price - b.price);
+        break;
+      case "high to low":
+        sortedItems.sort((a, b) => b.price - a.price);
+        break;
+      default:
+        break;
     }
+    setFilteredItems(sortedItems);
+  };
 
-    // show all data function
-    const showAll = () => {
-        setFilteredItems(menu)
-        setSelectedCategory("all")
-    }
-
-    // sorting base on A-Z , Z-A,  Low- High price
-    const handleSortChange = (option) => {
-        setSortOption(option)
-
-        let sortedItems = [...filteredItems]
-
-        // logic
-        switch (option) {
-            case "A-Z":
-                sortedItems.sort((a, b) => a.name.localeCompare(b.name))
-                break;
-            case "Z-A":
-                sortedItems.sort((a, b) => b.name.localeCompare(a.name))
-                break;
-            case "low-to-high":
-                sortedItems.sort((a, b) => a.price - b.price)
-                break;
-            case "high to low":
-                sortedItems.sort((a, b) => b.price - a.price)
-                break;
-            default:
-                break;
-        }
-        setFilteredItems(sortedItems);
-    }
-
-
-
-    return (
-        <div>
-            {/* menu banner */}
-            <div className='section-container bg-gradient-to-r from-[#FAFAFA] to-[#FCFCFC]'>
-                <div className='py-48 flex flex-col  md:flex-row justify-center items-center '>
-
-                    {/* text div */}
-                    <div className=' px-4 space-y-7 text-center  '>
-                        <h2 className='text-3xl md:text-5xl lg:text-6xl px-16 font-semibold'>Dive into Delights Of Delectable <span className='text-green'>Food</span> </h2>
-                        <p className='text-[#4A4A4A] text-2xl px-16 my-5 md:w-4/5 mx-auto'>Where Each Plate Weaves a Story of Culinary Mastery and Passionate Craftsmanship</p>
-                        <div className='px-16 flex items-center gap-6 '>
-                            <button className="btn bg-green rounded-full text-white px-5 mx-auto">Order Now</button>
-
-                        </div>
-                    </div>
-
-                </div>
+  return (
+    <div>
+      {/* menu banner */}
+      <div className="section-container bg-gradient-to-r from-[#FAFAFA] to-[#FCFCFC]">
+        <div className="py-48 flex flex-col  md:flex-row justify-center items-center ">
+          {/* text div */}
+          <div className=" px-4 space-y-7 text-center  ">
+            <h2 className="text-3xl md:text-5xl lg:text-6xl px-16 font-semibold">
+              Dive into Delights Of Delectable{" "}
+              <span className="text-green">Food</span>{" "}
+            </h2>
+            <p className="text-[#4A4A4A] text-2xl px-16 my-5 md:w-4/5 mx-auto">
+              Where Each Plate Weaves a Story of Culinary Mastery and Passionate
+              Craftsmanship
+            </p>
+            <div className="px-16 flex items-center gap-6 ">
+              <button className="btn bg-green rounded-full text-white px-5 mx-auto">
+                Order Now
+              </button>
             </div>
-
-            {/* menu shop section */}
-            <div className='section-container'>
-
-            </div>
+          </div>
         </div>
-    );
+      </div>
+
+      {/* menu shop section */}
+      <div className="section-container">
+        <div className="flex items-center justify-between">
+          {/* filtering and sorting  */}
+          <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap my-4">
+            {/* all category btn */}
+            <button
+              onClick={showAll}
+              className={selectedCategory === "all" ? "active" : ""}
+            >
+              All
+            </button>
+            <button
+              onClick={() => filterItem("salad")}
+              className={selectedCategory === "salad" ? "active" : ""}
+            >
+              Salad
+            </button>
+
+            <button
+              onClick={() => filterItem("pizza")}
+              className={selectedCategory === "pizza" ? "active" : ""}
+            >
+              Pizza
+            </button>
+            <button
+              onClick={() => filterItem("soup")}
+              className={selectedCategory === "soup" ? "active" : ""}
+            >
+              Soups
+            </button>
+            <button
+              onClick={() => filterItem("dessert")}
+              className={selectedCategory === "dessert" ? "active" : ""}
+            >
+              Desserts
+            </button>
+            <button
+              onClick={() => filterItem("drinks")}
+              className={selectedCategory === "drinks" ? "active" : ""}
+            >
+              Drinks
+            </button>
+          </div>
+
+          {/* sorting filtering */}
+          <div className="flex ">
+            <div className="bg-black p-2 ">
+              <FaFilter className="h-4 w-4 text-white" />
+            </div>
+
+            {/* sorting option */}
+            <select
+              name="sort"
+              id="sort"
+              onChange={(e) => handleSortChange(e.target.value)}
+              value={sortOption}
+              className="text-white bg-black px-2 py-1 rounded-sm"
+            >
+              <option value="default">Default</option>
+              <option value="A-Z">A-Z</option>
+              <option value="Z-A">Z-A</option>
+              <option value="low-to-high">Low to High Price</option>
+              <option value="high-to-low">High to Low Price</option>
+            </select>
+          </div>
+        </div>
+
+        {/* product card */}
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {filteredItems.map((item) => (
+            <Cards key={item._id} item={item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -1,26 +1,31 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { FaHeart } from "react-icons/fa";
+import { useState } from 'react';
 
 const Cards = ({ item }) => {
-    const [isHeartFillted, setIsHeartFillted] = useState(false);
+    const [isHeartFilled, setIsHeartFilled] = useState(false);
 
-    const handleHeatClick = () => {
-        setIsHeartFillted(!isHeartFillted)
-    }
+    const handleHeartClick = () => {
+        setIsHeartFilled(!isHeartFilled);
+    };
+
     return (
         <div>
             {/* cards */}
             <div className="card w-96 bg-base-100 shadow-xl relative">
-                <div className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-green ${isHeartFillted ? "text-rose-500" : "text-white"} `} onClick={handleHeatClick}>
+                <div className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-green ${isHeartFilled ? "text-rose-500" : "text-white"} `} onClick={handleHeartClick}>
                     <FaHeart className='h-5 w-5 cursor-pointer ' />
                 </div>
-                <Link to={'/menu/${item._id}'}>
-                    <figure><img src={item.image} alt="" className='hover:scale-105 transition-all duration-200 md:h-72' /></figure>
+                <Link to={`/menu/${item._id}`}>
+                    <figure>
+                        <img src={item.image} alt="" className='hover:scale-105 transition-all duration-200 md:h-72' />
+                    </figure>
                 </Link>
                 <div className="card-body">
-                    <Link to={'/menu/${item._id}'}><h2 className="card-title">{item.name}</h2></Link>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <Link to={`/menu/${item._id}`}>
+                        <h2 className="card-title">{item.name}</h2>
+                    </Link>
+                    <p>If a dog chews shoes, whose shoes does he choose?</p>
                     <div className="card-actions justify-between items-center mt-2">
                         <h5 className='font-semibold'><span className='text-sm text-red'>${item.price}</span></h5>
                         <button className="btn  bg-green text-white">Buy Now</button>
@@ -28,7 +33,7 @@ const Cards = ({ item }) => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Cards
+export default Cards;
